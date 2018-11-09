95dabf163b1f88802854e9e4633fd9d13c91cc96
Carito Orellana <carito@Caritos-MacBook-Pro.local>
2018-11-09 13:21:03 -0800

    Add constants for product content URIs

--- app/src/main/java/com/example/carito/inventoryapp/data/ProductContract.java
+++ app/src/main/java/com/example/carito/inventoryapp/data/ProductContract.java
@@ -1,5 +1,6 @@
 package com.example.carito.inventoryapp.data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 /**
@@ -9,10 +10,34 @@ public class ProductContract {
     private ProductContract(){}
 
     /**
+     * The "Content authority" is a name for the entire content provider, similar to the
+     * relationship between a domain name and its website.  A convenient string to use for the
+     * content authority is the package name for the app, which is guaranteed to be unique on the
+     * device.
+     */
+    public static final String CONTENT_AUTHORITY = "com.example.carito.inventoryapp";
+    /**
+     * Use CONTENT_AUTHORITY to create the base of all URI's which apps will use to contact
+     * the content provider.
+     */
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+    /**
+     * Possible path (appended to base content URI for possible URI's)
+     * For instance, content://com.example.carito.inventoryapp/products/ is a valid path for
+     * looking at product data. content://com.example.carito.inventoryapp/stock/ will fail,
+     * as the ContentProvider hasn't been given any information on what to do with "stock".
+     */
+    public static final String PATH_PRODUCTS = "productos";
+
+    /**
      * Inner class that defines constant values for the products database table.
      * Each entry in the table represents a single product.
      */
     public static final class ProductEntry implements BaseColumns{
+        /** The content URI to access the product data in the provider */
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_PRODUCTS);
+
+
         /** Name of database table for products */
         public static final String TABLE_NAME = "products";
 
