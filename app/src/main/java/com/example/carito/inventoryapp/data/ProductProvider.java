cc1decdd6c77dcadb343b49191cf1838a1e9271d
Carito Orellana <carito@Caritos-MacBook-Pro.local>
2018-11-09 14:31:52 -0800

    Implement ContentProvider query() method

--- app/src/main/java/com/example/carito/inventoryapp/data/ProductProvider.java
+++ app/src/main/java/com/example/carito/inventoryapp/data/ProductProvider.java
@@ -1,19 +1,27 @@
 package com.example.carito.inventoryapp.data;
 
 import android.content.ContentProvider;
+import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
+import com.example.carito.inventoryapp.data.ProductContract.ProductEntry;
+
 
 /**
  * {@link ContentProvider} for Inventory app.
  */
 public class ProductProvider extends ContentProvider {
 
-    /** URI matcher code for the content URI for the products table */
+    /**
+     * URI matcher code for the content URI for the products table
+     */
     private static final int PRODUCTS = 100;
-    /** URI matcher code for the content URI for a single pet in the products table */
+    /**
+     * URI matcher code for the content URI for a single pet in the products table
+     */
     private static final int PRODUCT_ID = 101;
     /**
      * UriMatcher object to match a content URI to a corresponding code.
@@ -21,6 +29,7 @@ public class ProductProvider extends ContentProvider {
      * It's common to use NO_MATCH as the input for this case.
      */
     private static final UriMatcher sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
     // Static initializer. This is run the first time anything is called from this class.
     static {
         // The calls to addURI() go here, for all of the content URI patterns that the provider
@@ -39,7 +48,10 @@ public class ProductProvider extends ContentProvider {
         // "content://com.example.carito.inventoryapp/products" (without a number at the end) doesn't match.
         sUriMatcher.addURI(ProductContract.CONTENT_AUTHORITY, ProductContract.PATH_PRODUCTS + "/#", PRODUCT_ID);
     }
-    /** Tag for the log messages */
+
+    /**
+     * Tag for the log messages
+     */
     public static final String LOG_TAG = ProductProvider.class.getSimpleName();
 
     /**
@@ -62,7 +74,40 @@ public class ProductProvider extends ContentProvider {
     @Override
     public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs,
                         String sortOrder) {
-        return null;
+        // Get readable database
+        SQLiteDatabase database = mDbHelper.getReadableDatabase();
+        // This cursor will hold the result of the query
+        Cursor cursor;
+        // Figure out if the URI matcher can match the URI to a specific code
+        int match = sUriMatcher.match(uri);
+        switch (match) {
+            case PRODUCTS:
+                // For the PRODUCTS code, query the products table directly with the given
+                // projection, selection, selection arguments, and sort order. The cursor
+                // could contain multiple rows of the products table.
+                cursor = database.query(ProductEntry.TABLE_NAME, projection, selection, selectionArgs,
+                        null, null, sortOrder);
+                break;
+            case PRODUCT_ID:
+                // For the PRODUCT_ID code, extract out the ID from the URI.
+                // For an example URI such as "content://com.example.carito.inventoryapp/products/3",
+                // the selection will be "_id=?" and the selection argument will be a
+                // String array containing the actual ID of 3 in this case.
+                //
+                // For every "?" in the selection, we need to have an element in the selection
+                // arguments that will fill in the "?". Since we have 1 question mark in the
+                // selection, we have 1 String in the selection arguments' String array.
+                selection = ProductEntry._ID + "=?";
+                selectionArgs = new String[]{String.valueOf(ContentUris.parseId(uri))};
+                // This will perform a query on the products table where the _id equals 3 to return a
+                // Cursor containing that row of the table.
+                cursor = database.query(ProductEntry.TABLE_NAME, projection, selection, selectionArgs,
+                        null, null, sortOrder);
+                break;
+            default:
+                throw new IllegalArgumentException("Cannot query unknown URI " + uri);
+        }
+        return cursor;
     }
 
     /**
@@ -77,7 +122,8 @@ public class ProductProvider extends ContentProvider {
      * Updates the data at the given selection and selection arguments, with the new ContentValues.
      */
     @Override
-    public int update(Uri uri, ContentValues contentValues, String selection, String[] selectionArgs) {
+    public int update(Uri uri, ContentValues contentValues, String selection, String[]
+            selectionArgs) {
         return 0;
     }
 
@@ -96,8 +142,6 @@ public class ProductProvider extends ContentProvider {
     public String getType(Uri uri) {
         return null;
     }
+
+
 }
-
-
-
-
