89103f3aedd0598e72612f8260905b9d26ae3b0f
Carito Orellana <carito@Caritos-MacBook-Pro.local>
2018-11-09 19:28:14 -0800

    Implement ContentProvider getType method

--- app/src/main/java/com/example/carito/inventoryapp/data/ProductContract.java
+++ app/src/main/java/com/example/carito/inventoryapp/data/ProductContract.java
@@ -1,5 +1,6 @@
 package com.example.carito.inventoryapp.data;
 
+import android.content.ContentResolver;
 import android.net.Uri;
 import android.provider.BaseColumns;
 
@@ -37,6 +38,16 @@ public class ProductContract {
         /** The content URI to access the product data in the provider */
         public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_PRODUCTS);
 
+        /**
+         * The MIME type of the {@link #CONTENT_URI} for a list of products.
+         */
+        public static final String CONTENT_LIST_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_PRODUCTS;
+        /**
+         * The MIME type of the {@link #CONTENT_URI} for a single pet.
+         */
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_PRODUCTS;
 
         /** Name of database table for products */
         public static final String TABLE_NAME = "products";
